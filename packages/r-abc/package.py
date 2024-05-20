860585ea81aba55dfcf738dd34d4ef63618c3c33
Michael Woolnough <mw31@sanger.ac.uk>
2024-05-20 08:00:38 +0000

    Make recipe fail during build, not during dependency resolution.

--- packages/r-abc/package.py
+++ packages/r-abc/package.py
@@ -19,10 +19,3 @@ class RAbc(RPackage):
 	cran = "abc" 
 
 	version("2.2.1", md5="21e4c928a8cdd4c6fe3c1c76c99913a9")
-
-	depends_on("r@2.10:", type=("build", "run"))
-	depends_on("r-abc-data", type=("build", "run"))
-	depends_on("r-nnet", type=("build", "run"))
-	depends_on("r-quantreg", type=("build", "run"))
-	depends_on("r-mass", type=("build", "run"))
-	depends_on("r-locfit", type=("build", "run"))
